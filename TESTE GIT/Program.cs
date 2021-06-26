9c311af997e464fea7762aa4ab9926c920a3a62d
Bruno Rocha <bruno.trabalho229243@outlook.com>
2021-06-26 13:01:28 -0300

    Agora o programa emite um BOM DIA

--- TESTE GIT/Program.cs
+++ TESTE GIT/Program.cs
@@ -4,6 +4,7 @@ namespace TESTE_GIT {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia !");
             Console.ReadKey();
         }
     }
