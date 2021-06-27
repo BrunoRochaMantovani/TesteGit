6a4d0d46bfbb9b66331c8a409bbb16e6bb79b89c
Bruno Rocha <bruno.trabalho229243@outlook.com>
2021-06-26 22:00:50 -0300

    RETIRADO A FUNCIONALIDADE HELLO WORLD

--- TESTE GIT/Program.cs
+++ TESTE GIT/Program.cs
@@ -3,7 +3,7 @@
 namespace TESTE_GIT {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+         
             Console.WriteLine("Bom dia !");
             Console.WriteLine("Tudo bem ?");
             Console.WriteLine();
